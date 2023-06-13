6906b5bd9ec956a3fcbf0241634270bcb8722a55
MatyTsoraro <maty.tsoraro@gmail.com>
2023-06-13 23:15:51 +0300

    lets go

---
+++ client/src/App.js
@@ -0,0 +1,63 @@
+import './App.css';
+
+function App() {
+    return (
+        <main>
+            <header>
+                <a href="" className="logo">MyBlog</a>
+                <nav>
+                    <a href="">Login</a>
+                    <a href=""> Register</a>
+
+                </nav>
+
+
+            </header>
+            <div className="post">
+                <div className = "image"><img src="https://techcrunch.com/wp-content/uploads/2023/06/CMC_6538.jpg?w=430&h=230&crop=1" alt="" />
+                </div>
+                <div className="texts">
+                    <h2>Putting the ‘Pro’ in Apple Vision Pro and other TC news</h2>
+                    <p className = "info">
+                        <a className ="author">Maty Tsoraro</a>
+                        <time>2023-06-13 22:44</time>
+                    </p>
+                    <p className="summary">At WWDC this week, Apple revealed its new Vision Pro headset, a platform it described as the advent of spatial computing</p>
+                </div>
+            </div>
+            <div className="post">
+                <div className = "image"><img src="https://techcrunch.com/wp-content/uploads/2023/06/CMC_6538.jpg?w=430&h=230&crop=1" alt="" />
+                </div>
+                <div className="texts">
+                    <h2>Putting the ‘Pro’ in Apple Vision Pro and other TC news</h2>
+                    <p className = "info">
+                        <a className ="author">Maty Tsoraro</a>
+                        <time>2023-06-13 22:44</time>
+                    </p>
+                    <p className="summary">At WWDC this week, Apple revealed its new Vision Pro headset, a platform it described as the advent of spatial computing</p>
+                </div>
+            </div>
+            <div className="post">
+                <div className = "image"><img src="https://techcrunch.com/wp-content/uploads/2023/06/CMC_6538.jpg?w=430&h=230&crop=1" alt="" />
+                </div>
+                <div className="texts">
+                    <h2>Putting the ‘Pro’ in Apple Vision Pro and other TC news</h2>
+                    <p className = "info">
+                        <a className ="author">Maty Tsoraro</a>
+                        <time>2023-06-13 22:44</time>
+                    </p>
+                    <p className="summary">At WWDC this week, Apple revealed its new Vision Pro headset, a platform it described as the advent of spatial computing</p>
+                </div>
+            </div>
+
+
+
+
+
+        </main>
+
+
+    );
+}
+
+export default App;
